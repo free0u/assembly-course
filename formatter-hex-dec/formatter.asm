b53ab9320cae848e1a1a57e9419b17eb721bcb86
Anton Evdokimov <free0u@gmail.com>
2013-03-15 21:44:47 +0400

    Print 3 arguments (first is filename)

--- formatter-hex-dec/formatter.asm
+++ formatter-hex-dec/formatter.asm
@@ -5,9 +5,29 @@ section .text
 global _main
 
 _main:
-    push text
-    call _printf
-    add esp, 4
+        mov ebx, esp
+        add ebx, 4
+
+        mov ebx, [ebx]
+       
+        mov     ebx, [esp + 4]
+        
+        mov     ebx, [esp + 8]
+ 
+        mov     eax, [ebx]
+        push    eax
+        call    _printf
+        
+        mov     eax, [ebx + 4]
+        push    eax
+        call    _printf
+        
+        mov     eax, [ebx + 8]
+        push    eax
+        call    _printf
+        
+        
+    add esp, 12
     ret
     
 section .rodata
