c1205f7f090471c5920c399d09787a01054ffea5
Anton Evdokimov <free0u@gmail.com>
2013-03-16 02:45:09 +0400

    Parse sign in first bit of input hex number

--- formatter-hex-dec/formatter.asm
+++ formatter-hex-dec/formatter.asm
@@ -4,7 +4,23 @@ extern _printf
 section .text
 
 global _main
+jmp _main
 
+; input al
+; output al
+hex_char_to_byte:
+    or al, 0x20
+    
+    cmp al, 0x61 ; 'a' == 0x61
+    jb if_is_digit ; al < 'a' then digit else letter
+if_is_letter:
+    sub al, 0x61 ; al -= 'a'
+    add al, 10 ; al += 10
+    ret
+if_is_digit:
+    sub al, 0x30 ; al -= '0'
+    ret
+    
 
 _main:
     ; parse sign of arg2
@@ -29,6 +45,43 @@ _main:
 not_negative:
 
 
+
+    ; number is really negative?
+    ; TODO think about length
+    mov eax, [start_num_arg]
+    mov al, [eax]
+    call hex_char_to_byte
+    and al, 0x0A ; cmp al, 0b1000
+    cmp al, 0x0A
+    
+    je if_negative_code
+if_positive_code:
+    mov al, [is_negative]
+    
+    jmp end_if_negative_positive_code
+if_negative_code:
+    mov al, [is_negative]
+    xor al, 0xFF
+end_if_negative_positive_code:    
+    mov [is_negative], al
+    
+    
+    
+    mov al, [is_negative]
+    cmp al, 0xFF
+    
+    je print_negative
+print_positive:
+    push text_positive
+    call _printf
+    add esp, 4
+    ret
+print_negative:
+    push text_negative
+    call _printf
+    add esp, 4
+    
+    
     ret
     
     
@@ -40,3 +93,12 @@ start_num_arg dd 0
  
 section .rodata
 text db "Hello, world", 0
+
+text_negative db "negative",0
+text_positive db "positive",0
+
+
+
+
+
+
