6ebc430f7c1761c61d4c63e2f4242381e2bd61d0
Anton Evdokimov <free0u@gmail.com>
2013-06-18 01:14:06 +0400

    Added dumping double's bits

--- double2string/main.c
+++ double2string/main.c
@@ -1,8 +1,46 @@
 #include <cstdio>
+#include <iostream>
+#include <string.h>
 
-int main(int argc, char ** argv) {
-    for (int i = 0; i < argc; ++i) {
-        printf("%s\n", argv[i]);
-    }        
+using namespace std;
+
+int bit(char c, int i)
+{
+    int res = c & (1 << i);
+    return res == 0 ? 0 : 1;
+}
+
+void print_char_bin(char c)
+{
+    for (int i = 0; i < 8; ++i) {
+        printf("%d", bit(c, 8 - i - 1));
+        if (i == 3) printf(" ");
+    }
+}
+
+void double2str(double *in, char *out_buf)
+{
+    sprintf(out_buf, "%f", *in);
+    char* s = (char*)malloc(8);
+    memcpy(s, in, 8);
+
+    for (int i = 0; i < 8; ++i)
+    {
+        print_char_bin(s[8 - i - 1]);
+        printf(" ");
+    }
+
+    free(s);
+}
+
+int main(int argc, char ** argv)
+{
+    (void)argc;(void)argv;
+
+    char buf[100];
+    double d = -0.0;
+    double* pd = &d;
+
+    double2str(pd, buf);
     return 0;
 }
